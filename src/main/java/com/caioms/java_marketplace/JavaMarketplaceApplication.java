7cccd0d018af701180aa449b3fb721d0620e6d5a
Caio Marques Silva <caiocpe@gmail.com>
2026-06-26 03:17:55 -0300

    Project creation

---
+++ src/main/java/com/caioms/java_marketplace/JavaMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.caioms.java_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caioms/java_marketplace/JavaMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caioms.java_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
