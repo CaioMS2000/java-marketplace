302c7f88256af06bc562e2a13273c21e288b1c7d
Caio Marques Silva <caiocpe@gmail.com>
2026-06-26 18:45:30 -0300

    chore: wire component scan and externalized application config

--- src/main/java/com/caioms/java_marketplace/JavaMarketplaceApplication.java
+++ src/main/java/com/caioms/java_marketplace/JavaMarketplaceApplication.java
@@ -2,12 +2,13 @@ package com.caioms.java_marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.caioms")
 public class JavaMarketplaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaMarketplaceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(JavaMarketplaceApplication.class, args);
+  }
 }
